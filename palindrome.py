a02bd0554b8158c71a4269a5db090fdccefa0c01
rjtazree@gmail.com <rjtazree@gmail.com>
2020-08-06 13:25:43 +0600

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+#palindrome
+def isPalindrome(str): 
+  
+    # Run loop from 0 to len/2  
+    for i in range(0, int(len(str)/2)):  
+        if str[i] != str[len(str)-i-1]: 
+            return False
+    return True
+
+s = input()
+ans = isPalindrome(s) 
+  
+if (ans): 
+    print("Yes") 
+else: 
+    print("No") 
